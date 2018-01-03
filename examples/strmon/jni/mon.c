5be8c455491c08f10491160a217da9f90980ff1b
病已 <leilei.yll@alibaba-inc.com>
2018-01-03 17:23:11 +0800

    增加全局锁，应对thread.start的竞争问题

--- examples/strmon/jni/mon.c
+++ examples/strmon/jni/mon.c
@@ -39,7 +39,7 @@
 
 static struct hook_t eph;
 static struct dexstuff_t d;
-
+static long threadObjArray;
 static int debug;
 
 static void my_log(char *msg)
@@ -56,24 +56,43 @@ static struct dalvik_hook_t threadDHT;
 
 static struct dalvik_hook_t threadSNT;
 
-static struct dalvik_hook_t threadCRT;
+static char startFuncEnterLog[50] = "thread_hook_start(enter)";
+static char startFuncExitLog[50] = "thread_hook_start(exit)";
+
+static pthread_mutex_t mutex;
 
 static void* thread_hook_start(JNIEnv *env, jobject obj)
 {
-
-	LOGE("thread_hook_start()\n")
-	LOGE("env = 0x%x\n", env)
-	LOGE("obj = 0x%x\n", obj)
+	pthread_mutex_lock(&mutex);
+	LOGE("%s", startFuncEnterLog);
+	// LOGE("env = 0x%x\n", env)
+	// LOGE("obj = 0x%x\n", obj)
 	dalvik_prepare(&d, &threadDHT, env);
 	(*env)->CallVoidMethod(env, obj, threadDHT.mid); 
-	LOGE("success calling : %s\n", threadDHT.method_name)
 	dalvik_postcall(&d, &threadDHT);
+	LOGE("%s\n", startFuncExitLog);	
+	pthread_mutex_unlock(&mutex);
+	jclass threadClass = (*env)->GetObjectClass(env, obj);
+	jfieldID threadNameId = (*env)->GetFieldID(env, threadClass, "name", "Ljava/lang/String;");
+	jstring threadNameJs = (jstring)(*env)->GetObjectField(env, obj, threadNameId);
+	if(threadNameJs) {
+		const char* threadNameChars = (*env)->GetStringUTFChars(env, threadNameJs, 0);
+		if(threadNameChars) {
+			LOGE("Thread Name : %s", threadNameChars);
+			(*env)->ReleaseStringUTFChars(env, threadNameJs, threadNameChars); 
+		}
+	}
+
+	jclass  throwable_class = (*env)->FindClass(env, "java/lang/Throwable");
+	jmethodID  throwable_init = (*env)->GetMethodID(env, throwable_class, "<init>", "()V");
+	jobject throwable_obj = (*env)->NewObject(env, throwable_class, throwable_init);
+	jmethodID throwable_mid = (*env)->GetMethodID(env, throwable_class, "printStackTrace", "()V");
+	(*env)->CallVoidMethod(env, throwable_obj, throwable_mid);
 	return 0;
 }
 
 static void* thread_hook_setName(JNIEnv *env, jobject obj, jobject threadName)
 {
-
 	LOGE("thread_hook_setName()\n")
 	// LOGE("env = 0x%x\n", env)
 	// LOGE("obj = 0x%x\n", obj)
@@ -86,29 +105,6 @@ static void* thread_hook_setName(JNIEnv *env, jobject obj, jobject threadName)
 	return 0;
 }
 
-static void* thread_hook_create(JNIEnv *env, 
-								jobject obj,
-								jobject threadGroup,
-								jobject runnable,
-								jobject threadName,
-								jlong stackSize) {
-	LOGE("in thread_hook_create: %%x%x\n", obj);
-	LOGE("threadGroup: %%x%x\n", threadGroup);
-	LOGE("runnable: %%x%x\n", runnable);
-	LOGE("threadName: %%x%x\n", threadName);
-	LOGE("stackSize: %%x%x\n", stackSize);
-	dalvik_prepare(&d, &threadCRT, env);
-	jvalue args[4];
-	args[0].l = threadGroup;
-	args[1].l = runnable;
-	args[2].l = threadName;
-	args[3].j = stackSize;
-	(*env)->CallVoidMethodA(env, obj, threadCRT.mid, args); 
-	LOGE("success calling : %s\n", threadCRT.method_name)
-	dalvik_postcall(&d, &threadCRT);
-	return 0;
-}
-
 void do_patch()
 {
 	LOGE("do_patch()\n")
@@ -118,20 +114,10 @@ void do_patch()
 	threadDHT.dump = 0;
 	dalvik_hook(&d, &threadDHT);
 
-	dalvik_hook_setup(&threadSNT, "Ljava/lang/Thread;",  "setName",  "(Ljava/lang/String;)V", 2, thread_hook_setName);
-	threadSNT.debug_me = 0;
-	threadSNT.dump = 0;
-	dalvik_hook(&d, &threadSNT);
-
-	dalvik_hook_setup(&threadCRT, 
-					  "Ljava/lang/Thread;",  
-					  "create",  
-					  "(Ljava/lang/ThreadGroup;Ljava/lang/Runnable;Ljava/lang/String;J)V", 
-					  5, 
-					  thread_hook_create);
-	threadCRT.debug_me = 1;		
-	threadCRT.dump = 1;		  
-	dalvik_hook(&d, &threadCRT);
+	// dalvik_hook_setup(&threadSNT, "Ljava/lang/Thread;",  "setName",  "(Ljava/lang/String;)V", 2, thread_hook_setName);
+	// threadSNT.debug_me = 0;
+	// threadSNT.dump = 0;
+	// dalvik_hook(&d, &threadSNT);
 
 	LOGE("do_patch: after dalvik_hook");
 }
@@ -171,6 +157,8 @@ void my_init(void)
 	set_logfunction(my_log2);
 	// set log function for libdalvikhook (very important!)
 	dalvikhook_set_logfunction(my_log2);
+
+	pthread_mutex_init(&mutex, NULL);
 
     hook(&eph, getpid(), "libc.", "epoll_wait", my_epoll_wait, 0);
 }
